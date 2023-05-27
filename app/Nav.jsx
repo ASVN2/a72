a3fa0fdc60e176f27d6822532bb0099efb42f16b
A72 <93783198+ASVN2@users.noreply.github.com>
2023-05-27 13:38:06 +0100

    hot fix

--- app/Nav.jsx
+++ app/Nav.jsx
@@ -34,17 +34,6 @@ const Nav = () => {
           <MdWbSunny className="text-2xl" />
         </div>
       </ul>
-      <script
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-id="AA72"
-        data-description="Support me on Buy me a coffee!"
-        data-message=""
-        data-color="#40DCA5"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"></script>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <div className="page mx-auto max-w-[1000px]  mt-16">
-      <p className="text-2xl mb-6 font-sans">🚚 👷 Working on it </p>
-      <div className="mx-auto bg-gray-900 rounded-md max-w-[1000px] text-2xl font-mono text-green-400 border-2 border-dashed border-white p-8 ">
-        {' '}
-        Hey there!👋 <span className="font-bold text-white">Abdelahdi A72</span> here. Exciting news—I've launched my own website! It's a window into my world, showcasing my work
-        and passions. Join me on this journey, explore my projects, and let's reconnect. Share your stories, achievements, and thoughts. Together, let's inspire and support each
-        other to reach new heights!
-      </div>
+      <p className="text-2xl font-mono underline underline-white">About me </p>
+      <p className="mt-4">
+        I'm <span className="text-green-500 font-bold text-xl">[</span> Abdelhadi <span className="font-bold">AKA</span> A72{' '}
+        <span className="text-green-500 font-bold text-xl">]</span>, a front-end developer specializing in JavaScript, React, CSS, HTML, Sass, and Tailwind CSS. On this site, I
+        share my knowledge, tricks, and progress to keep motivated and inspire others in their front-end development journey. Let's build beautiful web experiences and foster a
+        supportive community. Welcome aboard!
+      </p>
     </div>
   );
 }
